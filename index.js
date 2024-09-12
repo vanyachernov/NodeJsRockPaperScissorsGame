9e6e1f32fbe6157ba1ccf643148a8d545fb7df5d
Vanya Chernov <vanyachernov2001@gmail.com>
2024-09-12 12:49:01 +0300

    Add RuleChecker for validation input values

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const RuleChecker = require('./config/ruleChecker');
+
 const args = process.argv.slice(2);
 
-if (args.length < 3 || args.length % 2 === 0 || new Set(args).size !== args.length) {
-    console.error('Error: You must provide an odd number of unique moves (at least 3)!');
-    process.exit();
+let ruleChecker = new RuleChecker(args);
+if (ruleChecker.IsValidInput() && ruleChecker.IsActionInput())
+{
+    console.log("vse ok");
 }
+
